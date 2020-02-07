3afc2d95bda6728696658eb0ea1344e9471c7358
Abhishek Gupta <hi@itsabhishek.me>
2020-02-08 01:47:24 +0530

    minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+
 import pkg from './package.json';
 
 const name = pkg.name
@@ -17,6 +19,7 @@ export default {
     svelte({
       customElement: true,
     }),
-		resolve()
+    resolve(),
+    terser(),
 	]
 };
