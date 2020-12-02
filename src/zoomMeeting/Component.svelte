f5e6165cdcd70f9439b949f730c8987bbcc09810
Jatin Katyal <jatin.katyal13@gmail.com>
2020-12-02 16:35:39 +0530

    add: zoom meeting component

---
+++ src/zoomMeeting/Component.svelte
@@ -0,0 +1,56 @@
+<svelte:options tag="zoom-meeting" />
+
+<svelte:head>
+  <script src="https://source.zoom.us/1.8.3/lib/vendor/react.min.js"></script>
+  <script src="https://source.zoom.us/1.8.3/lib/vendor/react-dom.min.js"></script>
+  <script src="https://source.zoom.us/1.8.3/lib/vendor/redux.min.js"></script>
+  <script src="https://source.zoom.us/1.8.3/lib/vendor/redux-thunk.min.js"></script>
+  <script src="https://source.zoom.us/1.8.3/lib/vendor/jquery.min.js"></script>
+  <script src="https://source.zoom.us/1.8.3/lib/vendor/lodash.min.js"></script>
+  <script src="https://source.zoom.us/zoom-meeting-1.8.3.min.js" bind:this={script}></script>
+</svelte:head>
+
+<script>
+  import { onMount } from 'svelte';
+
+  let script;
+  
+  export let meetingnumber;
+  export let signature;
+  export let apikey;
+  export let username;
+  export let useremail;
+
+	onMount(async () => {
+		script.addEventListener('load', () => {
+      console.log(meetingnumber)
+      ZoomMtg.preLoadWasm();
+      ZoomMtg.prepareJssdk();
+      function beginJoin() {
+        ZoomMtg.init({
+          leaveUrl: 'http://localhost:8000',
+          success: function () {
+            ZoomMtg.join({
+              meetingNumber: meetingnumber,
+              userName: username,
+              signature,
+              apiKey: apikey,
+              userEmail: useremail,
+              success: function (res) {
+                console.log('success')
+              },
+              error: function (res) {
+                console.log(res);
+              },
+            });
+          },
+          error: function (res) {
+            console.log(res);
+          },
+        });
+      }
+      beginJoin();
+    })
+	});
+
+</script>
