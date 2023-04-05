aec8c2e6537d5e47c6bf0de7adc85f133592d5ea
Vibhu Dujari <vdvibhu20@gmail.com>
2023-04-05 15:42:03 +0530

    black theme integration for cb-navbar

--- src/cbNavbar/Component.svelte
+++ src/cbNavbar/Component.svelte
@@ -1,27 +1,27 @@
 <svelte:options tag="cb-navbar" />
 
-<div class="a-hb d-md-block d-none">
+<div class="a-hb d-md-block d-none bg-background-main-3 white">
 
   <div class="border">
     <div class="d-flex {topStyle} py-3 pr-5">
-      <span class="px-3 pointer" on:click={toggleExpanded}>
+      <span class="px-3 pointer position-relative {activeTab === "courses" && "active-tab"}" on:click={() => {toggleExpanded(); activeTab = "courses";}}>
         Courses
         <img 
           src="https://minio.cb.lk/public/fa-5.13/svgs/solid/chevron-{expanded ? 'up' : 'down'}.svg" 
           alt="chevron" 
           class="d-inline s-10x10"/>
       </span>
-      <div class="px-3">
-        <a href="https://codingblocks.com/about.html" target="_blank">About Us</a>
+      <div class="px-3 position-relative {activeTab === "about" && "active-tab"}">
+        <a class="white" href="https://codingblocks.com/about.html" target="_blank" on:click={() => activeTab = "about"}>About Us</a>
       </div>
-      <div class="px-3">
-        <a href="https://codingblocks.com/resources" target="_blank">Resources</a>
+      <div class="px-3 position-relative {activeTab === "resources" && "active-tab"}">
+        <a class="white" href="https://codingblocks.com/resources" target="_blank" on:click={() => activeTab = "resources"}>Resources</a>
       </div>
-      <div class="px-3">
-        <a href="https://codingblocks.com/campus-ambassador-program.html" target="_blank">Campus Ambassadors</a>
+      <div class="px-3 position-relative {activeTab === "ca" && "active-tab"}">
+        <a class="white" href="https://codingblocks.com/campus-ambassador-program.html" target="_blank" on:click={() => activeTab = "ca"}>Campus Ambassadors</a>
       </div>
-      <div class="px-3">
-        <a href="https://codingblocks.com/centres/" target="_blank">Contact Us</a>
+      <div class="px-3 position-relative {activeTab === "contact" && "active-tab"}">
+        <a class="white" href="https://codingblocks.com/centres/" target="_blank" on:click={() => activeTab = "contact"}>Contact Us</a>
       </div>
       <slot name="header"></slot>
     </div>
@@ -34,7 +34,7 @@
         <div class="py-5 font-md bold hover-grey">
           <div class="font-lg my-4 offset-3">Our Courses</div>
         </div>
-        <div class="py-4 font-md bold hover-grey {tab == 'online' && 'selected'}"  on:click={() => tab = "online"}>
+        <div class="py-4 font-md bold hover-grey {activeCoursesTab == 'online' && 'selected'}"  on:click={() => activeCoursesTab = "online"}>
           <div class="offset-3">
             <div>Online Courses</div>
             <a href="https://online.codingblocks.com/courses" target="_blank" class="font-mds orange"> 
@@ -42,7 +42,7 @@
             </a>
           </div>
         </div>
-        <div class="py-4 font-md bold hover-grey {tab == 'classroom' && 'selected'}" on:click={() => tab = "classroom"}>
+        <div class="py-4 font-md bold hover-grey {activeCoursesTab == 'classroom' && 'selected'}" on:click={() => activeCoursesTab = "classroom"}>
           <div class="offset-3">
             <div>Classroom Courses</div>
             <a href="https://codingblocks.com/classroom-courses/" target="_blank" class="font-mds orange"> 
@@ -54,7 +54,7 @@
         
       </div>
       <div class="col-9 font-mds">
-        {#if tab=="classroom"}
+        {#if activeCoursesTab=="classroom"}
           <ClassroomCourses />
         {:else}
           <OnlineCourses />
@@ -71,8 +71,9 @@
   // by default use latest motley version for unpkg
   export let topalign = "center"
   export let expanded = false
-  export let tab = "online"
-  export let css = "https://unpkg.com/@coding-blocks/motley/dist/app.min.css"
+  export let activeTab = ""
+  export let activeCoursesTab = "online"
+  export let css = "https://unpkg.com/@coding-blocks/motley/dist/online-cb/app.min.css"
 
   let topStyle = 'justify-content-center'
 
@@ -97,6 +98,17 @@
 
     .a-hb {
       display: none;
+    }
+
+    .active-tab:after{
+      content: "";
+      width: 98%;
+      height: 2px;
+      position: absolute;
+      left: -2px;
+      bottom: -16px;
+      display: block;
+      background: linear-gradient(90deg, #FB8530 0%, #FEB50B 100%);
     }
     
 </style>
