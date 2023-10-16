9faa8b8ef79b3810a7bacc55cbc3a1c2e543fea5
Vibhu Dujari <vdvibhu20@gmail.com>
2023-10-17 00:32:06 +0530

    remove motley from head

--- src/cbLoginSignup/Component.svelte
+++ src/cbLoginSignup/Component.svelte
@@ -3,7 +3,6 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://unpkg.com/@coding-blocks/motley@2.8.6/dist/account/app.min.css">
 </svelte:head>
 
 
@@ -195,6 +194,8 @@
   }
   
 </script>
+
+<link rel="stylesheet" href="https://unpkg.com/@coding-blocks/motley@2.8.6/dist/account/app.min.css">
 <style>
   input {
     border: none;
